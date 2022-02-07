6fd4ca4732e834d425e37f4df5d0ca996d82d271
barbar <n23>
2022-02-06 20:31:24 -0600

    init v2

--- src/main/java/dev/bar/sq/ListenerC.java
+++ src/main/java/dev/bar/sq/ListenerC.java
@@ -1,6 +1,7 @@
 package dev.bar.sq;
 
 import org.bukkit.ChatColor;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -8,14 +9,24 @@ import org.bukkit.event.player.PlayerJoinEvent;
 
 public class ListenerC implements Listener {
 
+    Main plugin;
+
+    public ListenerC(Main instance) {
+        plugin = instance;
+    }
+
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event)
     {
-
         Player p = event.getPlayer();
-        event.setJoinMessage(p.getDisplayName() + ChatColor.BLACK + " has joined !");
+        event.setJoinMessage(ChatColor.BLACK + p.getDisplayName() + ChatColor.BLACK + " has joined!");
 
     }
+
+
+
+
+
 
 }
 
--- src/main/java/dev/bar/sq/Main.java
+++ src/main/java/dev/bar/sq/Main.java
@@ -1,6 +1,7 @@
 package dev.bar.sq;
 
 import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -8,9 +9,13 @@ public final class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        this.getServer().getPluginManager().registerEvents(new ListenerC(), this);
+        final FileConfiguration config = this.getConfig();
+
+        PluginManager pm = getServer().getPluginManager();
+        pm.registerEvents(new ListenerC(this), this);
 
     }
+
 
     @Override
     public void onDisable() {
